2f2090593060221e7fad2814c327c805192d12c4
DeveshChau <deveshachaudhari@gmail.com>
2015-10-31 10:54:44 +0530

    pin CRUD operations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031042905) do
+ActiveRecord::Schema.define(version: 20151031043251) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
