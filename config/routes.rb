783fe8d81e900070234bf81814302b1d98e18fad
DeveshChau <deveshachaudhari@gmail.com>
2015-10-31 12:57:30 +0530

    styling forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :pins 
+  devise_for :users
+  resources :pins
   root "pins#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
